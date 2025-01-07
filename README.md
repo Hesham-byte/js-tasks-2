608fd7df0ea3d3dbbd74da2b10cb85f4b314b4fb
Hesham Fawzy <h3050571@gmail.com>
2025-01-07 21:15:08 +0200

    add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+![Screenshot20250107at90841PM.png](assets/Screenshot 2025-01-07 at 9.08.41 PM.png)
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: "https://hesham-byte.github.io/js-tasks-2/",
+    build: {
+        minify: false,
+    }
+});
